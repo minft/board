5e1b475cc482f846793ca3821699c75ce00eaeaa
minft <klm0225@naver.com>
2024-03-13 22:50:04 +0900

    feat: 게시글 상세 조회
    - 게시글 상세 조회 기능 추가
    - detail 페이지 추가
    - 상세 진입 시 조회 수 증가 추가

--- src/main/java/com/minft/board/repository/BoardRepository.java
+++ src/main/java/com/minft/board/repository/BoardRepository.java
@@ -2,6 +2,15 @@ package com.minft.board.repository;
 
 import com.minft.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    // update board_table set board_hits=board_hits+1 where id=? // mysql 기준 쿼리
+    // 기존의 조회수 에서 +1 해주는 쿼리, 아래는 DB가 아니라 Entity를 기준으로함
+    @Modifying
+    @Query(value = "update BoardEntity b set b.boardHits=b.boardHits+1 where b.id=:id")
+    void updateHits(@Param("id") Long id);
+    // update, delete 등의 쿼리를 실행해야할 때는 @Modifying을 필수로 붙여야한다고 함
 }
