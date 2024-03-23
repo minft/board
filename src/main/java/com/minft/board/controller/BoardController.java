897e16b58d63e4c901253aae660fb6739cb02381
minft <klm0225@naver.com>
2024-03-23 16:29:05 +0900

    feat: 게시글 삭제 기능 추가
    - 게시글 삭제 처리
    - 삭제 후 목록으로 이동

--- src/main/java/com/minft/board/controller/BoardController.java
+++ src/main/java/com/minft/board/controller/BoardController.java
@@ -39,9 +39,9 @@ public class BoardController {
     @GetMapping("/{id}")
     public String findById(@PathVariable Long id, Model model) {
         /*
-        * 해당 게시글의 조회수를 하나 올리고
-        * 게시글 데이터를 가져와서 detail.html에 출력
-        * */
+         * 해당 게시글의 조회수를 하나 올리고
+         * 게시글 데이터를 가져와서 detail.html에 출력
+         * */
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
@@ -63,4 +63,9 @@ public class BoardController {
 //        return "redirect:/board/" + boardDTO.getId(); // 이렇게 선언하면 수정해도 조회수가 올라갈 수 있음
     }
 
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
 }
