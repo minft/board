67d2576b520371fefbd02419d7ef79feab5d10e0
minft <klm0225@naver.com>
2024-04-03 22:03:04 +0900

    feat: 댓글 작성, 저장 처리 추가
    - 댓글 테이블 생성, boardId에 따라 저장되게 하는 처리 추가

--- src/main/java/com/minft/board/dto/CommentDTO.java
+++ src/main/java/com/minft/board/dto/CommentDTO.java
@@ -1,5 +1,6 @@
 package com.minft.board.dto;
 
+import com.minft.board.entity.CommentEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,4 +16,16 @@ public class CommentDTO {
     private String commentContents;
     private Long boardId;
     private LocalDateTime commentCreatedTime;
+
+    public static CommentDTO toCommentDTO(CommentEntity commentEntity, Long boardId) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(commentDTO.getId());
+        commentDTO.setCommentWriter(commentEntity.getCommentWriter());
+        commentDTO.setCommentContents(commentEntity.getCommentContents());
+        commentDTO.setCommentCreatedTime(commentEntity.getCreatedTime());
+//        commentDTO.setBoardId(commentEntity.getId()); // Service 메서드에 @Transactional 을 선언해줘야함
+        commentDTO.setBoardId(boardId);
+        return commentDTO;
+    }
+
 }
---
+++ src/main/java/com/minft/board/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.minft.board.repository;
+
+import com.minft.board.entity.BoardEntity;
+import com.minft.board.entity.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    //    select * from comment_table where board_id = ? order by id desc;
+    List<CommentEntity> findAllByBoardEntityOrderByIdDesc(BoardEntity boardEntity);
+
+}
