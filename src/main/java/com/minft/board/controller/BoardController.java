8543d9c51269529dd1830b32c07225d0d3444ac3
minft <klm0225@naver.com>
2024-04-04 23:32:55 +0900

    feat: 게시글 상세에서 댓글목록 조회 추가
    - 게시글 상세에서 작성한 댓글 목록 조회할 수 있는 기능 추가

--- src/main/java/com/minft/board/controller/BoardController.java
+++ src/main/java/com/minft/board/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.minft.board.controller;
 
 import com.minft.board.dto.BoardDTO;
+import com.minft.board.dto.CommentDTO;
 import com.minft.board.service.BoardService;
+import com.minft.board.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +20,7 @@ import java.util.List;
 @RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
+    private final CommentService commentService;
 
     @GetMapping("/save")
     public String saveForm() {
@@ -49,6 +52,9 @@ public class BoardController {
          * */
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
+//        댓글 목록 가져오기
+        List<CommentDTO> commentDTOList = commentService.findAll(id);
+        model.addAttribute("commentService", commentDTOList);
         model.addAttribute("board", boardDTO);
         model.addAttribute("page", pageable.getPageNumber());
         return "detail";
